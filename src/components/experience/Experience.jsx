9b076e4f106eb207fe60ad43832d50b3d2d222d1
Santiago397 <santilopez397@gmail.com>
2024-05-07 14:09:11 -0500

    time-line experience completed

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,10 +3,11 @@ import './Experience.css'
 import experiences from '../../utils/experiences.json'
 import { motion } from "framer-motion";
 import Chips from '../chips/Chips'
+import Timeline from '../timeline/Timeline';
 
 const Experience = () => {
   return (
-    <section className="flexCenter paddings innerWidth e-wrapper">
+    <section className="paddings innerWidth e-wrapper">
       <motion.article 
         variants={{
           hidden: { opacity: 0, x: -75 },
@@ -18,32 +19,34 @@ const Experience = () => {
         className="flexColStart e-container"
       >
         <h5 className="title">Experiencia</h5>
-        {
-          experiences.map((exp) => {
-            return (
-              <article key={exp.company} className='flexColStart innerWidth e-content'>
-                <div className="flexColStart e-subtitle">
-                  <h6>{exp.company}</h6>
-                  <span>{exp.role}</span>
-                  <span>{exp.start} - {exp.end}</span>
-                </div>
-                <ul className='e-duties'>
-                  {
-                    exp.duties.map((duty, idx) => <li key={idx} >{duty}</li>)
-                  }
-                </ul>
-                <div className='flexCenter e-chips'>
-                  {
-                    exp.tech.map((t, idx) => <Chips key={idx} text={t} />)
-                  }
-                </div>
-              </article>
-            )
-          })
-        }
+        <div className="e-list">
+          {
+            experiences.map((exp) => {
+              return (
+                <article key={exp.company} className='flexColStart innerWidth e-content'>
+                  <div className="flexColStart e-subtitle">
+                    <h6>{exp.company}</h6>
+                    <span>{exp.role}</span>
+                    <span>{exp.start} - {exp.end}</span>
+                  </div>
+                  <ul className='e-duties'>
+                    {
+                      exp.duties.map((duty, idx) => <li key={idx} >{duty}</li>)
+                    }
+                  </ul>
+                  <div className='flexCenter e-chips'>
+                    {
+                      exp.tech.map((t, idx) => <Chips key={idx} text={t} />)
+                    }
+                  </div>
+                </article>
+              )
+            })
+          }
+        </div>
       </motion.article>
-      <article className="time-line">
-        {/* TODO create timeline */}
+      <article className="flexColCenter innerWidth  time-line">
+        <Timeline />
       </article>
     </section>
   )
