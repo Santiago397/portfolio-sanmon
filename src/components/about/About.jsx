c3749c5641e4af085f328034e0cd89f7b1141bda
Santiago397 <santilopez397@gmail.com>
2024-05-17 10:55:02 -0500

    desc updated and cv download created

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,15 +3,25 @@ import { slides } from "../../utils/slides-images";
 import {
   FaLinkedin,
   FaGithub,
-  FaCopy
+  FaCopy,
+  FaDownload
 } from "react-icons/fa";
 import { motion } from "framer-motion";
 import "./About.css";
 import Carousel from "../carousel/Carousel";
+import { Toaster, toast } from "sonner";
 
 const About = () => {
+
+  const copyText = async () => {
+    await navigator.clipboard.writeText('santilopez397@gmail.com')
+    .then(() => toast.success("Se ha copiado con éxito!"))
+    .catch(() => toast.error("No se ha logrado copiar"))
+  }
+
   return (
     <section className="flexCenter paddings innerWidth a-wrapper">
+      <Toaster />
       <motion.article
         variants={{
           hidden: { opacity: 0, x: -75 },
@@ -24,18 +34,18 @@ const About = () => {
       >
         <h5 className="title">About</h5>
         <p>
-          Web developer with <strong>one year</strong> of experience in the
-          industry, passionate about technology and digital innovations. <br />
+          Desarrollador web con <strong>un año</strong> de experiencia en el sector,
+          apasionado por la tecnología y las innovaciones digitales <br />
         </p>
         <p>
-          Throughout this time, I have demonstrated skills in{" "}
-          <strong>assertive communication, teamwork, and flexibility</strong> to
-          adapt to daily challenges.
+          Durante este tiempo, he demostrado habilidades en{" "}
+          <strong>comunicación asertiva, trabajo en equipo y flexibilidad </strong> 
+          para adaptarme a los retos diarios. 
         </p>
         <p>
-          I am enthusiastic about collaborating in dynamic teams, applying my
-          technical knowledge, and unleashing my creativity to achieve
-          exceptional results.
+          Me entusiasma colaborar en equipos dinámicos,
+          aplicar mis conocimientos técnicos y dar rienda suelta a mi
+          creatividad para lograr resultados excepcionales.
         </p>
         <div className="flexStart a-social">
           <a href="https://www.linkedin.com/in/sanmon12/" target="_blank">
@@ -44,10 +54,16 @@ const About = () => {
           <a href="https://github.com/Santiago397" target="_blank">
             <FaGithub title="Github" size={30} />
           </a>
+          <a href="/gl_santiago_monrroy_cv.pdf" target="_blank">
+            <button className="btn-cv">
+              CV
+              <FaDownload />
+            </button>
+          </a>
         </div>
         <div className="flexStart a-mail">
           <span>santilopez397@gmail.com</span>
-          <FaCopy title="Copiar" />
+          <FaCopy title="Copiar" onClick={copyText} />
         </div>
       </motion.article>
       <article className="flexColCenter innerWidth a-images">
